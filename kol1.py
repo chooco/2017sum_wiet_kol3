f58fd3e7398aaeefcdd98feedac788b7c5df8995
chooco <kllaudiaa@gmail.com>
2017-04-10 15:52:43 +0200

    add some test

---
+++ kol1.py
@@ -0,0 +1,17 @@
+import time
+
+from simulation import Plane, Simulation
+
+
+if __name__ == "__main__":
+    angle = float(raw_input("Enter current angle\n"))
+    plane = Plane(angle)
+    simulation = Simulation(plane)
+    
+    while True:
+        try:
+            simulation.next_step()
+            time.sleep(2)
+        except KeyboardInterrupt:
+            simulation.end_of_simulation()
+            break
---
+++ test.py
@@ -0,0 +1,29 @@
+#source_katarzynaadr
+
+import unittest
+from simulation import Plane, Simulation
+import math
+import random
+from mock import Mock
+
+
+class testTask(unittest.TestCase):
+    simulate = Simulation()
+    plane = Plane()
+
+    def correct_tilt_test(self):
+        expected = self.angle - self.angle
+        self.assertEqual()
+        
+
+    def generate_tilt_test(self):
+        pass
+    def simulate_test(self):
+        pass
+
+    def next_step_test(self):
+        self.assertEqual(next(self.simulation))
+
+    def end_of_simulation_test(self):
+        pass
+
