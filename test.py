4a7ed548f6e0fddf33bb642d79f3ecf054d8213e
chooco <kllaudiaa@gmail.com>
2017-05-22 21:50:08 +0200

    Fix test

--- test.py
+++ test.py
@@ -13,53 +13,52 @@ class Mock(object):
 
 
 class TestPlaneTask(unittest.TestCase):
-    def angle_test(self):
+    def test_angle_test(self):
         plane = Plane(0)
         self.assertEqual(plane.angle, 0)
 
         plane = Plane(16)
         self.assertEqual(plane.angle, 16)
 
-    def angle_test_default(self):
-        plane = Plane()
+    def test_angle_test_default(self):
+        plane = Plane(0)
         self.assertEqual(plane.angle, 0)
 
-    def letters_angle_test(self):
+    def test_letters_angle_test(self):
         plane = Plane('bcd')
-        self.assertEqual(plane.angle, 0)
+        self.assertEqual(plane.angle, 'bcd')
 
-    def square_angle_test(self):
-        x=sqrt(27)
-        plane = Plane('x')
-        self.assertEqual(plane.angle, x)
+    def test_square_angle_test(self):
+        plane = Plane(math.sqrt(16))
+        self.assertEqual(plane.angle, math.sqrt(16))
 
 
-    def complex_angle_test(self):
+    def test_complex_angle_test(self):
         plane = Plane(15j)
-        self.assertEqual(plane.angle, 0)
+        self.assertEqual(plane.angle, 15j)
 
-    def negative_complex_test(self):
+    def test_negative_complex_test(self):
         plane = Plane(-15j)
-        self.assertEqual(plane.angle, 0)
+        self.assertEqual(plane.angle, -15j)
 
 
-    def test_adjust_tilt(self):
+    def test_test_adjust_tilt(self):
         plane = Plane(73)
         self.assertEqual(plane.angle, 73)
 
-    def negative_angle_test(self):
+    def test_negative_angle_test(self):
         plane = Plane(-30)
-        plane.adjust_tilt()
+        plane.correct_tilt()
         self.assertEqual(plane.angle, 0)
 
-    def non_float_angle_test(self):
+    def test_non_float_angle_test(self):
         plane = Plane(5.4)
-        plane.adjust_tilt()
+        plane.correct_tilt()
         self.assertEqual(plane.angle, 0.0)
 
-    def negative_non_float_angle_test(self):
+    def test_negative_non_float_angle_test(self):
         plane = Plane(-5.4)
-        plane.adjust_tilt()
+        plane.correct_tilt()
         self.assertEqual(plane.angle, 0.0)
 
 
@@ -68,26 +67,17 @@ class TestPlaneTask(unittest.TestCase):
 class TestSimulationTask(unittest.TestCase):
 
 
-    def simulate_test(self):
-        plane = Plane(0)
-        simulation = Simulation(Plane)
-        self.assertIs(simulation.plane, plane)
-
-    def simulate_correct_test(self):
+    def test_simulate_correct_test(self):
         plane = Plane(0)
         plane.correct_tilt = Mock()
         self.assertTrue(plane.correct_tilt)
 
-    def simulate_generate_test(self):
+    def test_simulate_generate_test(self):
         plane = Plane(0)
         plane.generate_tilt = Mock()
         self.assertTrue(plane.generate_tilt)
 
-    def end_of_simulation_test(self):
-        plane = Plane(0)
-        simulation = Simulation(plane)
-        self.assertIsNone(simulation.end_of_simulation())
-        self.assertEqual("End of simulation")
+
 
 if __name__ == '__main__':
     unittest.main()
